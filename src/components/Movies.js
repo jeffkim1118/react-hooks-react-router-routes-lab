ff46d2bf5fefcf25d06bf973a9a1969e44710f78
Jeffrey Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-01-12 09:52:06 -0500

    In Progress

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(
+        ({title, time, genres}) => 
+        <div>
+          <h1>Name: {title}</h1>
+          <p>Time: {time}</p>
+          <p>Genres:</p>
+          <ul>
+            <li>{genres[0]}</li>
+            <li>{genres[1]}</li>
+            <li>{genres[2]}</li>
+          </ul>
+        </div>)}
+
+    </div>
+  )
 }
 
 export default Movies;
